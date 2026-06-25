caf7e8b13a1d035c44ffd808dbefff6892f30bd0
BUDDHABHUSHAN WAGHMARE <buddhabhushan.waghmare.2322@gmail.com>
2026-06-25 11:39:01 +0530

    Add: find-minimum-in-rotated-sorted-array - submission-0

---
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-0.py
@@ -0,0 +1,6 @@
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        # V.1.0
+        # This will be the brute force method 
+        # simple way is to look up in the arrya 
+        return min(nums)
